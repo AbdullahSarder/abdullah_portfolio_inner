da56cfa907b5753bcecd8461d584c7458233dcbf
Abdullah <57371591+AbdullahSarder@users.noreply.github.com>
2023-06-21 13:52:55 +1000

    update exprience

--- src/components/showcase/Experience.tsx
+++ src/components/showcase/Experience.tsx
@@ -10,7 +10,7 @@ const Experience: React.FC<ExperienceProps> = (props) => {
             <div style={styles.headerContainer}>
                 <div style={styles.header}>
                     <div style={styles.headerRow}>
-                        <h1>Nest <Go></Go></h1>
+                        <h1>Nest Go</h1>
                         <a
                             rel="noreferrer"
                             target="_blank"
